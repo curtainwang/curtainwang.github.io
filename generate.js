84d6216a4d4719e4f6d39934de942c2ac7127b82
curtainwang <curtainwang@qq.com>
2016-11-20 17:16:54 +0800

    2016/11/20 17:16:54

--- generate.js
+++ generate.js
@@ -35,3 +35,5 @@ function getFileInFolder(path){
 	}
 		
 }
+
+
--- test.py
+++ test.py
@@ -1,26 +1,43 @@
 import os
 import platform
 
-# generate my site's files
-
+'''
 dir="./"
+f=open(r'./file.txt', 'w') #g_file="file.txt"
+
+
+# generate my site's files
 for root, dirs, files in os.walk(dir):
 	for file in files:
 		print os.path.join(root, file)
 
-# write the answer to a text file
-dir="."
-g_file="file.txt"
 
 for root, dirs, files in os.walk(dir, topdown=False):
 	for file in files:
 		print os.path.join(root, file)
 
-f=open(r'./file.txt', 'w')
+
 f.write('hello')
 f.writelines(['nihao', 'sb'])
+f.close()
+'''
+
+def get_file(dir_name, file_name):
+	generate_file = open(file_name, 'w') 
+
+	for file in os.listdir(dir_name):
+		generate_file.write(file)
+		generate_file.write('\n')
+
+	generate_file.close()
+	return
 
 
+
+get_file("./html/", "file_html.txt")
+get_file("./txt/", "file_txt.txt")
+
+'''
 # check the operating system
 if platform.system() == "Windows":
 	print("this is windows")
@@ -41,3 +58,4 @@ os.system("git add .")
 os.system("set tmp=%date:~0,10% %time:~0,8%")
 os.system("git commit -m "%tmp%"")
 os.system("git push")
+'''
